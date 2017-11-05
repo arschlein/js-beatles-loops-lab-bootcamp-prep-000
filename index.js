e064ab43959bf6e38ec99335746cf6628227d46b
Monika <arschlein@seznam.cz>
2017-11-05 13:37:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musicians, instruments){
-  var newArray = []
+  const newArray = []
   for(let i= 0, l = musicians.length; i < l; i++){
     newArray.push(`${musicians[i]} plays ${instruments[i]}.`)
   }
