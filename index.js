6f9157c447adffc3662c675aaf2e9e5878b2e389
Monika <arschlein@seznam.cz>
2017-11-05 13:51:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ function johnLennonFacts(facts){
   }
   return cryFacts
 }
+function iLoveTheBeatles(n) {
+  const arr = []
+
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+  return arr
+}
